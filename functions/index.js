84adee607fc7fba37f78dc1a540321f2bfc7923e
David Chango <davidmineplay@gmail.com>
2020-06-15 09:02:06 -0500

    Changes

--- functions/index.js
+++ functions/index.js
@@ -12,12 +12,6 @@ admin.initializeApp({
 });
 
 =======
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://desarrollo-web-5c200.firebaseio.com/"
-});
-
 >>>>>>> 1a5569ab04bb6bac623114d28bac559517d89f9f
 const database = admin.database();
 
@@ -77,28 +71,17 @@ function listPersonsByAge(){
 }
 
 ///========================= Funciones URLs ===================///
-app.post('/api/persons', function (req, res) {
-  let varName = req.body['name'];
-  let varAge = req.body['age'];
+
 =======
 ///========================= Funciones URLs ===================///
 app.post('/api/persons', function (req, res) {
-  let varName = req.body.name;
-  let varAge = req.body.age;
->>>>>>> 1a5569ab04bb6bac623114d28bac559517d89f9f
+  let varName = req.body['name'];
+  let varAge = req.body['age'];
   var person = {
     name : varName,
-    age : varAge  
-  };
+    age : varAge  };
   createPerson(person);
-  return res.status(201).json({ message: "Success person was added." });
-<<<<<<< HEAD
-});
-
-app.get('/api/persons/:id', function(req, res){
-  let varId = req.params.id;
-  retrievePerson(varId).then(result => {
-=======
+  return res.status(201).json({ message: "Success person was added.   :)" });
 });
 
 app.get('/api/persons/:id', function(req, res){
@@ -117,58 +100,21 @@ app.put('/api/persons/:id', function (req, res) {
     name : varName,
     age : varAge  };
   updatePerson(varId, person);
-  return res.status(200).json({ message: "Success person was updated." });
+  return res.status(200).json({ message: "Success person was updated.   :)" });
 });
 
 app.delete('/api/persons/:id',function(req, res){
   let varId = req.params.id;
   deletePerson(varId);
-  return res.status(200).json({ message: "Success person was deleted." });
+  return res.status(200).json({ message: "Success person was deleted.   -_-" });
 });
 
 app.get('/api/persons', function(req, res){
   listPersons().then(result => {
->>>>>>> 1a5569ab04bb6bac623114d28bac559517d89f9f
       return res.status(200).json(result); 
     }
   ).catch(err => console.log(err));
 });
-<<<<<<< HEAD
-
-app.put('/api/persons/:id', function (req, res) {
-  let varId = req.params.id;
-  let varName = req.body['name'];
-  let varAge = req.body['age'];
-  var person = {
-    name : varName,
-    age : varAge  };
-  updatePerson(varId, person);
-  return res.status(200).json({ message: "Success person was updated." });
-});
-
-app.delete('/api/persons/:id',function(req, res){
-  let varId = req.params.id;
-  deletePerson(varId);
-  return res.status(200).json({ message: "Success person was deleted." });
-});
-
-app.get('/api/persons', function(req, res){
-  listPersons().then(result => {
-=======
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-app.get('/api/person', function(req, res){
-  let varId = req.query.id;
-  retrievePerson(varId).then(result => {
->>>>>>> 1a5569ab04bb6bac623114d28bac559517d89f9f
-      return res.status(200).json(result); 
-    }
-  ).catch(err => console.log(err));
-});
-
-<<<<<<< HEAD
 
 app.get('/api/person', function(req, res){
   let varId = req.query.id;
@@ -177,9 +123,59 @@ app.get('/api/person', function(req, res){
     }
   ).catch(err => console.log(err));
 });
+////////////////////////////////////////////////////////////
+//=====CRUD SKILLS======////
+app.post('/api/persons/:id/skills', function (req, res) {
+ 
+  var skill = {
+    name :req.body['name'],
+    hours : req.body['hours'],
+    date: req.body['date'],
+    endorsed:req.body['endorsed']
+  };
+  createSkills(req.params.id,skill);
+  return res.status(201).json({ message: "Success skill was added to person.   :)" });
+});
 
+//eliminar
+app.delete('/api/persons/:id/skills/:idSkill',function(req, res){
+  let varId=req.params.id;
+  let varidSkill = req.params.idSkill;
+  deleteSkills(varId, varidSkill);
+  return res.status(200).json({ message: " Delete SKILLd.   -_-" });
+});
 
-//////////////////////////////////////////
+//actualizar
+app.put('/api/persons/:id/skills/:idSkills', function (req, res) {
+  let varId=req.params.id;
+  let varidSkills = req.params.idSkills;
+  var skill = {
+    name :req.body['name'],
+    hours : req.body['hours'],
+    date: req.body['date'],
+    endorsed:req.body['endorsed']
+  };
+  updateSkills(varId,varidSkills, skill);
+  return res.status(200).json({ message: "Se actualizo skills.   :)" });
+});
+//listar
+app.get('/api/persons/:id/skills', function(req, res){
+  listSkill(req.params.id).then(result => {
+      return res.status(200).json(result); 
+    }
+  ).catch(err => console.log(err));
+});
+
+//retrieve
+
+app.get('/api/persons/:id/skills/:idSkills', function(req, res){
+  let varId=req.params.id;
+  let varIdSkills = req.params.idSkills;
+  retrieveSkills(varId,varIdSkills).then(result => {
+      return res.status(200).json(result); 
+    }
+  ).catch(err => console.log(err));
+});
 
 
 
